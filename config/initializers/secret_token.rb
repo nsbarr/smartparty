bd8bfd15bfac10137adc012c1dc2a46daf5219fa
nsbarr <nsbarr@gmail.com>
2011-09-18 17:13:16 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smartparty::Application.config.secret_token = '522cf55d4545025684d9c5fa81bfe0c278212f5bfe79e30a21790d9edb4c5b401f72b756feed0f416d8721f7198180632fc3dfb4472aadaf029884a35f6601da'
